09ed679378c2266245bd5d306d5970e56fee5428
sachin kumar <sachin.kumar@quovantis.com>
2023-03-20 10:12:14 +0530

    added babylonjs with cuboid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Map, TileLayer } from "react-leaflet";
+import html2canvas from "html2canvas";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import CuboidWithTexture from "./CuboidWithTexture";
+
+import {
+  FreeCamera,
+  HemisphericLight,
+  MeshBuilder,
+  Vector3,
+  Texture,
+  StandardMaterial
+} from "@babylonjs/core";
+
+const defaultCenter = [38.9072, -77.0369];
+const defaultZoom = 8;
 
 function App() {
+  const [screenshot, setScreenshot] = useState();
+  const mapRef = useRef();
+
+  let box;
+
+  const onSceneReady = (scene) => {
+    var camera = new FreeCamera(
+      "camera1",
+      new Vector3(0, 5, -10),
+      scene
+    );
+    camera.setTarget(Vector3.Zero());
+    const canvas = scene.getEngine().getRenderingCanvas();
+    camera.attachControl(canvas, true);
+    var light = new HemisphericLight(
+      "light",
+      new Vector3(0, 1, 0),
+      scene
+    );
+    light.intensity = 0.7;
+    box = MeshBuilder.CreateBox("cuboid", { size: 3 }, scene);
+    box.position.y = 1;
+    const texture = new Texture(screenshot, scene);
+    const material = new StandardMaterial("texture", scene);
+    material.diffuseTexture = texture;
+    box.material = material;
+  };
+
+  const onRender = (scene) => {
+    if (box !== undefined) {
+      var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+      const rpm = 10;
+      box.rotation.y +=
+        (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+    }
+  };
+
+  async function handleScreenshot() {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+    const canvas = await html2canvas(map.getContainer(), {
+      useCORS: true,
+      allowTaint: true
+    });
+    let dataurl = canvas.toDataURL("image/png");
+    setScreenshot(dataurl);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Map ref={mapRef} center={defaultCenter} zoom={defaultZoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
+      <div className="sidebar">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={handleScreenshot}>Take Screenshot</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      {screenshot && (
+        <div className="modal">
+          <div className="close" ><a href="#" className="cross" onClick={()=>setScreenshot(null)}/></div>
+          <CuboidWithTexture
+            canvasId="babylon-canvas"
+            antialias
+            onSceneReady={onSceneReady}
+            onRender={onRender}
+          />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/CuboidWithTexture.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef } from "react";
+import {
+  Engine,
+  EngineOptions,
+  FreeCamera,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  Scene,
+  SceneOptions,
+  Vector3
+} from "@babylonjs/core";
+import * as BABYLON from "babylonjs";
+
+function CuboidWithTexture(props) {
+  const canvasRef = useRef(null);
+  const {
+    canvasId,
+    antialias,
+    engineOptions,
+    adaptToDeviceRatio,
+    sceneOptions,
+    onRender,
+    onSceneReady,
+    screenshot,
+    ...rest
+  } = props;
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const engine = new Engine(
+      canvasRef.current,
+      antialias,
+      engineOptions,
+      adaptToDeviceRatio
+    );
+    const scene = new Scene(engine, sceneOptions);
+    if (scene.isReady()) {
+      onSceneReady(scene);
+    } else {
+      scene.onReadyObservable.addOnce(onSceneReady);
+    }
+    engine.runRenderLoop(() => {
+      onRender(scene);
+      scene.render();
+    });
+
+    const resize = () => {
+      scene.getEngine().resize();
+    };
+
+    if (window) {
+      window.addEventListener("resize", resize);
+    }
+
+    return () => {
+      scene.getEngine().dispose();
+
+      if (window) {
+        window.removeEventListener("resize", resize);
+      }
+    };
+  }, [
+    antialias,
+    engineOptions,
+    adaptToDeviceRatio,
+    sceneOptions,
+    onRender,
+    onSceneReady
+  ]);
+
+  return <canvas className="canvas" id={canvasId} ref={canvasRef} {...rest} />;
+}
+export default CuboidWithTexture;
